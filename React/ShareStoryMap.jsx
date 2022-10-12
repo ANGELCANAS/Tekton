f8705b7cfb542cde2918b75c81c8cfc860c42714
Angel <107320037+ANGELCANAS@users.noreply.github.com>
2022-10-12 13:54:11 -0700

    Update ShareStoryMap.jsx

--- React/ShareStoryMap.jsx
+++ React/ShareStoryMap.jsx
@@ -1,35 +1,35 @@
 import React from 'react';
-import debug from "sabio-debug"
-import {useNavigate} from "react-router-dom"
-import PropTypes from "prop-types"
+import debug from 'sabio-debug';
+import { useNavigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-
-function ShareStoryMap(props){
-    const _logger = debug.extend("ShareStoryMap")
-    _logger("Share Story Map Logger is Working")
-    _logger("props", props)
+function ShareStoryMap(props) {
+    const _logger = debug.extend('ShareStoryMap');
+    _logger('Share Story Map Logger is Working');
+    _logger('props', props);
 
     const navigate = useNavigate();
 
-    const aStory = props.story
+    const aStory = props.story;
 
-    const onClickEdit = (e) =>{
+    const onClickEdit = (e) => {
         e.preventDefault();
-        navigate(`/story/edit`, {state: aStory})
-
-
-    }
+        const nav1 = { type: 'STORY_UPDATE', payload: aStory };
+        navigate(`/sharestory/${aStory.id}/`, { state: nav1 });
+    };
 
     return (
         <div className="mentoring-story-container">
             <div className="mentoring-story-title-container">
                 <div className="mentory-story-title">
                     {aStory.name}
-                   
-                    <button onClick={onClickEdit} type="button" className="btn btn-dark sseditbutton">Edit</button>
-                   
+
+                    {props.user.id === props.story.createdBy && (
+                        <button onClick={onClickEdit} type="button" className="btn btn-dark sseditbutton">
+                            Edit
+                        </button>
+                    )}
                 </div>
-                
             </div>
             <div className="share-story-content">
                 <div className="mentory-story-content-container col-6">
@@ -39,13 +39,12 @@ function ShareStoryMap(props){
                                 key={`share-story-image-${i}-${file.name}`}
                                 src={file.url}
                                 alt={file.name}
-                                className="col-6"
+                                className="story-image-container2"
                             />
                         ))}
                     </div>
                 </div>
-                <div className="mentorying-story-content-container col-6">{aStory.story}</div>
-               
+                <div className="mentorying-story-content-container1">{aStory.story}</div>
             </div>
         </div>
     );
@@ -53,13 +52,23 @@ function ShareStoryMap(props){
 
 ShareStoryMap.propTypes = {
     story: PropTypes.shape({
-       name: PropTypes.string.isRequired,
-       files: PropTypes.shape({
-            url: PropTypes.string.isRequired,
-            name: PropTypes.string.isRequired
-       }).isRequired,
-       story: PropTypes.string.isRequired,
-    })
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+        createdBy: PropTypes.number.isRequired,
+        files: PropTypes.arrayOf(
+            PropTypes.shape({
+                url: PropTypes.string.isRequired,
+                name: PropTypes.string,
+            })
+        ).isRequired,
 
-}
+        story: PropTypes.string.isRequired,
+    }),
+    user: PropTypes.shape({
+        email: PropTypes.string.isRequired,
+        id: PropTypes.number.isRequired,
+        isLoggedIn: PropTypes.bool.isRequired,
+        roles: PropTypes.arrayOf(PropTypes.string.isRequired),
+    }),
+};
 export default ShareStoryMap;
